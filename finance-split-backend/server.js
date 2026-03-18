70c73c1f31d5c2bf100cdbc7cd128f2ddcc1d40d
Rashi <23cs10ra112@gmail.com>
2026-03-18 12:39:36 +0530

    resolved network issue

--- finance-split-backend/server.js
+++ finance-split-backend/server.js
@@ -3,7 +3,8 @@ dotenv.config();
 
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-
+const cors = require('cors');
+app.use(cors());
 connectDB();
 
 const PORT = process.env.PORT || 5000;
